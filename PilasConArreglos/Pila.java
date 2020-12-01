f21867ae72add40bd9923cbc9689b33c94e0be2c
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2020-11-30 19:24:31 -0600

    Update Pila.java

--- PilasConArreglos/Pila.java
+++ PilasConArreglos/Pila.java
@@ -1,1 +1,36 @@
-
+package PilasConArreglos;
+import java.util.Scanner;
+public class Pila {
+    
+    Scanner leer = new Scanner(System.in);
+    String pilaNum[] = new String [5];    
+    int top = -1;
+    
+    public void Insertar(){
+        if(top >= pilaNum.length-1){
+            System.out.println("La pila está llena..");
+        }
+        else{
+            top +=1;
+            System.out.println("Ingrese una palabra: ");            
+            pilaNum[top] = leer.nextLine();
+        }
+    }
+    
+    public void Eliminar(){
+        if(top == -1){
+            System.out.println("La pila está vacía..");
+        }
+        else{
+            System.out.println("Se elimino un elemento de la pila..");
+            pilaNum[top] = "";
+            top--;
+        }        
+    }
+    
+    public void Mostrar(){
+        for(int top = 4; top >= 0; top--){
+            System.out.println("Datos de la pila: " +pilaNum[top]);            
+        }
+    }
+}
